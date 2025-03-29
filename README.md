844cd0d5eb0626a4dcd26a565d738f62081892e9
nelskev <youremail@yourdomain.com>
2025-03-29 17:51:08 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Django Notes App
+=====================================
+
+## Description
+---------------
+
+This is a project using React and Django and is a general notes app. All credit goes to TechWithTim and the tutorial can be watched here : https://youtu.be/c-QsfbznSXI&t=272
---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css"
+import LoadingIndicator from "./loadingIndicator";
+
+function Form({ route, method }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const name = method === "login" ? "Login" : "Register";
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+    
+        try { 
+            console.log('Attempting registration to:', route);
+            const res = await api.post(route, { username, password })
+            console.log('Response:', res);
+    
+            if (method === "login") {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate("/");
+            } else {
+                // Remove the console.error line since there's no error
+                navigate("/login");
+            }
+        }
+        catch (error) {
+            console.error('Error:', error);
+            alert(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+
+    return (
+     <form onSubmit={handleSubmit} className="form-container">
+        <h1>{name}</h1>
+        <input 
+        className="form-input"
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+        />
+        <input 
+        className="form-input"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        />
+        {loading && <LoadingIndicator />}
+        <button className="form-button" type="submit">
+            {name}
+        </button>
+    </form>
+);
+}
+
+export default Form
